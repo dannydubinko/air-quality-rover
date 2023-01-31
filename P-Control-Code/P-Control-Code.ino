496ecd1fa04c65459ff96c4989e45955e725471f
Luke-Major <luke.pb.major@gmail.com>
2023-01-31 14:58:21 -0500

    Create P-Control-Code.ino
    Added a p control

---
+++ P-Control-Code/P-Control-Code.ino
@@ -0,0 +1,206 @@
+/**
+ * @file ws1.ino
+ * @author Joshua Marshall (joshua.marshall@queensu.ca)
+ * @author Sabrina Button [updated] (sabrina.button@queensu.ca)
+ * @author Daniel Dubinko [updated] (daniel.dubinko@icloud.com)
+ * @author Luke Major [Updated] (luke.major@queensu.ca)
+ * @brief Arduino program to drive one wheel motor through a motor driver.
+ * @version 2.0 
+ * @date 2023-01-16
+ *
+ * @copyright Copyright (c) 2021-2022
+ *
+ */
+
+int EA = 19;  // Right Wheels PWM pin (must be a PWM pin).
+int I1 = 3;   // Right Wheels direction digital pin 1
+int I2 = 5;   // Right Wheels direction digital pin 2
+
+int EB = 18;  // Left Wheels PWM pin (must be a PWM pin)
+int I3 = 4;   // Left Wheels direction digital pin 1
+int I4 = 2;   // Left Wheels direction digital pin 2
+
+int left_Power_Input= 0;
+int right_Power_Input = 0;
+
+// Encoder ticks per (motor) revolution (TPR)
+const int TPR_left = 3083;   //3083
+const int TPR_right = 3100;  //3100
+
+// Wheel radius [m]
+const double RHO = 0.0625;
+
+// Variable to store estimated angular rate of left wheel [rad/s]
+double desired_Vehicle_Speed = 0;
+double desired_Omega = 0;
+double omega_Left = 0.0;
+double omega_Right = 0.0;
+double left_Velocity = 0.0;
+double right_Velocity = 0.0;
+
+// Sampling interval for measurements in milliseconds
+const int T = 1000;
+// Counters for milliseconds during interval
+long t_now = 0;
+long t_last = 0;
+
+// Encoder digital pins
+const byte LEFTWHEEL_A = 8;
+const byte LEFTWHEEL_B = 9;
+const byte RIGHTWHEEL_A = 10;
+const byte RIGHTWHEEL_B = 11;
+
+// Counter to keep track of encoder ticks [integer]
+volatile long encoder_ticks_left = 0;
+volatile long encoder_ticks_right = 0;
+
+// Counter to keep track of the last number of ticks [integer]
+long encoder_ticks_last_left = 0;
+long encoder_ticks_last_right = 0;
+
+// This function is called when SIGNAL_A goes HIGH
+void decodeEncoderTicksRight() {
+  if (digitalRead(RIGHTWHEEL_B) == LOW) {
+    // SIGNAL_A leads SIGNAL_B, so count one way
+    encoder_ticks_right--;
+  } else {
+    // SIGNAL_B leads SIGNAL_A, so count the other way
+    encoder_ticks_right++;
+  }
+}
+
+// This function is called when SIGNAL_B goes HIGH
+void decodeEncoderTicksLeft() {
+  if (digitalRead(LEFTWHEEL_B) == LOW) {
+    // SIGNAL_A leads SIGNAL_B, so count one way
+    encoder_ticks_left--;
+  } else {
+    // SIGNAL_B leads SIGNAL_A, so count the other way
+    encoder_ticks_left++;
+  }
+}
+
+void setup() {
+
+  // Open the serial port at 9600 bps
+  Serial.begin(9600);
+
+  // Set the pin modes for the encoders
+  pinMode(LEFTWHEEL_A, INPUT);
+  pinMode(LEFTWHEEL_B, INPUT);
+  pinMode(RIGHTWHEEL_A, INPUT);
+  pinMode(RIGHTWHEEL_B, INPUT);
+
+  // Configure digital pins for output
+  // RIGHT SIDE:
+  pinMode(EA, OUTPUT);
+  pinMode(I1, OUTPUT);
+  pinMode(I2, OUTPUT);
+
+  // LEFT SIDE:
+  pinMode(EB, OUTPUT);
+  pinMode(I3, OUTPUT);
+  pinMode(I4, OUTPUT);
+
+  // Every time SIGNAL_A goes HIGH, this is a pulse
+  attachInterrupt(digitalPinToInterrupt(LEFTWHEEL_A), decodeEncoderTicksLeft, RISING);
+  attachInterrupt(digitalPinToInterrupt(RIGHTWHEEL_A), decodeEncoderTicksRight, RISING);
+
+  // Print a message
+  Serial.print("Program initialized.");
+  Serial.print("\n");
+}
+
+void loop() {
+
+  int desired_Vehicle_Speed = 0;
+
+  int desired_Omega = 0;
+
+  do{
+
+  fwd(right_PI_Speed_Control(desired_Vehicle_Speed, desired_Omega), left_PI_Speed_Control(desired_Vehicle_Speed, desired_Omega));
+
+  } while (1);
+  
+} 
+
+void left_PI_Speed_Control(double desired_Vehicle_Speed, double desired_Omega) {
+
+  double desired_Left_Velocity = desired_Vehicle_Speed, - (1 / 2) * 0.2775 * desired_Omega;
+
+  double left_Power_Input = right_Proportional_Constant * (desired_Left_Velocity - compute_Left_Velocity());
+
+  return left_Power_Input;
+
+}
+
+double right_PI_Speed_Control(double desired_Velocity, double desired_Omega) {
+
+  double desired_Right_Velocity = desired_Veloctity + (1 / 2) * 0.2775 * desired_Omega;
+
+  double right_Power_Input = left_Proportional_Constant * (desired_Right_Velocity - compute_Right_Velocity());
+
+  return right_Power_Input;
+
+}
+
+double compute_Left_Velocity() {
+
+  t_now = millis();
+  int roverSpeed;
+  if (t_now - t_last >= T) {
+    // Estimate the rotational speed [rad/s]
+    left_Omega = 2.0 * PI * ((double)encoder_ticks_left / (double)TPR_left) * 1000.0 / (double)(t_now - t_last);
+  
+    left_Velocity = RHO * omega_L;
+
+    // Record the current time [ms]
+    t_last = t_now;
+
+    // Reset the encoder ticks counter
+    encoder_ticks_left = 0;
+
+    return left_Velocity;
+
+  }
+}
+
+double compute_Right_Velocity() {
+
+  t_now = millis();
+  int roverSpeed;
+  if (t_now - t_last >= T) {
+    // Estimate the rotational speed [rad/s]
+    right_Omega = 2.0 * PI * ((double)encoder_ticks_right / (double)TPR_right) * 1000.0 / (double)(t_now - t_last);
+    right_Velocity = RHO * omega_R;
+
+    // Record the current time [ms]
+    t_last = t_now;
+
+    // Reset the encoder ticks counter
+    encoder_ticks_right = 0;
+
+    return right_Velocity;
+  }
+}
+
+void fwd(int right_Power_Input, int left_Power_Input) {
+  // both sides drive forward.
+  motor_ctrl(right_Power_Input, left_Power_Input, LOW, HIGH, LOW, HIGH);
+}
+
+void motor_ctrl(int right_Power_Input, int left_Power_Input, bool i1, bool i2, bool i3, bool i4) {
+  // Input selected direction
+  digitalWrite(I1, i1);
+  digitalWrite(I2, i2);
+  digitalWrite(I3, i3);
+  digitalWrite(I4, i4);
+
+  // PWM control for each side of robot to the motor driver
+  analogWrite(EA, right_Power_Input);  // right side
+  analogWrite(EB, left_Power_Input);  // left side
+}
+
+
+
